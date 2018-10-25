1535056c34fcfc9e7b533f428a61c3afbfeea85d
rz0310 <43040390+rz0310@users.noreply.github.com>
2018-10-25 22:41:31 +0800

    SampleFrame
    SampleFrame

---
+++ SampleFrame/SampleFrame.java
@@ -0,0 +1,13 @@
+package homework;
+
+import java.awt.*;
+import java.awt.Event;
+import java.applet.*;
+
+public class SampleFrame extends Frame {
+	SampleFrame(String title){
+		super(title);
+	}
+	//MyWindowAdapter adapter = new MyWindowAdapter(this);
+	//addWindowListener(adapter);
+}
